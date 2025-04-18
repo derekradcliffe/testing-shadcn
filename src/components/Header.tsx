4c023dae841870705f3e11f594106fd4299fe9ee
Derek Radcliffe <dereksradcliffe@gmail.com>
2025-04-17 23:34:00 -0400

    init commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+// import { Link } from '@tanstack/react-router';
+
+export default function Header() {
+  return (
+    <header className="p-2 flex gap-2 bg-white text-black justify-between position-sticky top-0 z-10">
+      <nav className="flex flex-row mx-auto p-[1rem] bg-white">
+        {/* <div className="px-2 font-bold"> */}
+          {/* <Link to="/">Home</Link> */}
+        {/* </div> */}
+        <div className='title'>
+          <span className='text-4xl'>Menu This</span>
+        </div>
+      </nav>
+    </header>
+  )
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,56 @@
+import { createFileRoute } from '@tanstack/react-router';
+import data from '../../data.json';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from '@/components/ui/button';
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  const chunkArray = (array: any, size: any) => {
+    const result = [];
+  
+    for (let index = 0; index < array.length; index += size) {
+        result.push(array.slice(index, index + size));
+    }
+  
+    return result;
+  };
+
+  return (
+    <div className="App">
+      <div className='cardWrapper flex flex-col justify-evenly'>
+      {chunkArray(data.foods, 3).map((row, rowIndex) => (
+                <div className="flex md:flex-row flex-col mx-auto" key={rowIndex}>
+                    {row.map((item: any, index: any) => (
+                        <Card className="w-full max-w-sm mx-[1rem] mb-[1rem]" key={index}>
+                            <CardHeader>
+                                <img className='rounded-t-xl' src={item.image} alt="" />
+                            </CardHeader>
+                            <CardContent>
+                                <CardTitle>{item.name}</CardTitle>
+                                <CardDescription>{item.description}</CardDescription>
+                            </CardContent>
+                            <CardFooter>
+                                <p className="text-sm text-muted-foreground">{item.price}</p>
+                                <Button className="ml-auto cursor-pointer hover:bg-black hover:text-white" variant="outline">
+                                    Order
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    ))}
+                </div>
+            ))}
+      </div>
+    </div>
+  )
+}
