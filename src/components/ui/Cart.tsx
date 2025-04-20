72fbc70c9b188d46a9c3c9b7cb15cdd774183cc9
Derek Radcliffe <dereksradcliffe@gmail.com>
2025-04-20 10:06:24 -0400

    Updated button order and added badge to bag

--- src/components/ui/Cart.tsx
+++ src/components/ui/Cart.tsx
@@ -1,5 +1,3 @@
-import shoppingBag from "/src/img/shopping_bag.svg";
-
 import {
     Drawer,
     DrawerClose,
@@ -11,17 +9,17 @@ import {
     DrawerTrigger,
     DrawerTotal,
     DrawerClearTotal,
+    DrawerBag,
   } from "@/components/ui/drawer";
 
-// import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 const Cart = () => {
     return (
         <div className="cartWrapper cursor-pointer flex flex-row items-center justify-center">
             <Drawer>
-                <DrawerTrigger className="cursor-pointer">
-                    <img className="m-[1rem]" src={shoppingBag} alt="Shopping Bag" />
+                <DrawerTrigger className="cursor-pointer relative">
+                    <DrawerBag></DrawerBag>
                 </DrawerTrigger>
 
                 <DrawerContent>
@@ -31,10 +29,10 @@ const Cart = () => {
                         <DrawerTotal className="w-fit mx-auto font-bold" />
                         <div className="flex flex-row gap-2 items-center justify-center">
                             <Button className="w-fit cursor-pointer bg-black text-white border-black border-1 hover:bg-white hover:text-black hover:border-black hover:border-1">Submit</Button>
-                            <DrawerClearTotal></DrawerClearTotal>
                             <DrawerClose>
                                 <Button className="cursor-pointer bg-white text-black border-black border-1 hover:bg-black hover:text-white hover:border-s hover:border-1" variant="outline">Cancel</Button>
                             </DrawerClose>
+                            <DrawerClearTotal></DrawerClearTotal>
                         </div>
                     </DrawerFooter>
                 </DrawerContent>
